186f5d05d97bb07826a022f366828466cb3b753a
Christian Klutz <cklutz@gmail.com>
2024-10-08 18:32:10 +0200

    Fix test-docker-linux.ps1

--- test/test-docker-linux.ps1
+++ test/test-docker-linux.ps1
@@ -1,7 +1,7 @@
+#    -e VSTEST_HOST_DEBUG=1 `
 docker run `
     --rm --name LockCheck.Tests `
-    -e VSTEST_HOST_DEBUG=1 `
-    -v $PWD:/mnt/lc `
+    -v ${PWD}\..:/mnt/lc `
     -w /mnt/lc `
-    mcr.microsoft.com/dotnet/sdk:9.0 `
-    "dotnet build &&  dotnet test LockCheck.Tests\LockCheck.Tests.csproj"
+    mcr.microsoft.com/dotnet/sdk:8.0 `
+    bash -c '/usr/share/dotnet/dotnet test -c Release -f net8.0 test/LockCheck.Tests/LockCheck.Tests.csproj && /usr/share/dotnet/dotnet test -c Debug -f net8.0 test/LockCheck.Tests/LockCheck.Tests.csproj'
